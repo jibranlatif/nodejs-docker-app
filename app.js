8e165d0196323da8e380cdb00ad7bcdbe772b1b2
Jibran Latif <jirban.latif@gmail.com>
2025-05-03 15:27:54 +0800

    saad

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => res.send('Hello Saad ,Ayan and Maaz sojayo from Node.js app!'));
+app.get('/', (req, res) => res.send('Hello Muhammad Saad ,Ayan and Maaz sojayo from Node.js app!'));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
